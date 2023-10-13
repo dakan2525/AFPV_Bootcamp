dda5fcf7cfc5972fc754257ee987904d6794119b
dakan <andres25prado@gmail.com>
2023-10-13 10:26:15 -0500

    1.13*: anecdotes, paso 2

---
+++ part1/anecdotes/src/index.js
@@ -0,0 +1,53 @@
+import React, { useState } from 'react';
+import ReactDOM from 'react-dom/client';
+
+const App = () => {
+  const [selected, setSelected] = useState(0);
+  const [anecdotes, setAnecdotes] = useState([
+    {
+      content: "If it hurts, do it more often",
+      votes: 0,
+    },
+    {
+      content:"Adding manpower to a late software project makes it later!",
+      votes: 0,
+    },
+    {
+      content: 'The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
+      votes: 0,
+    },
+    {
+      content:'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
+      votes: 0,
+    },
+    {
+      content:'Premature optimization is the root of all evil.',
+      votes: 0,
+    },
+    {
+      content :'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.',
+      votes: 0,  
+    }
+  ]);
+
+
+  function vote() {
+    const updatedAnecdotes = [...anecdotes];
+    updatedAnecdotes[selected].votes++;
+    setAnecdotes(updatedAnecdotes); 
+  }
+
+
+  return (
+    <div>
+      {anecdotes[selected].content}
+      <p>Has {anecdotes[selected].votes} votes</p>
+      <button onClick={()=>vote()}>Vote</button>
+      <button onClick={()=>setSelected(Math.floor(Math.random() * 6))}>Next Anecdote</button>
+
+    </div>
+  );
+};
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<App />);
